4c21c3cddd7567f0c6026b6a1de5ae111984f9fa
Pehnny <theo.piron.pro@gmail.com>
2024-12-20 19:23:31 +0100

    Added Header style

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,20 +1,17 @@
 import {Link} from "react-router";
+import "./Header.css"
 
 export function Header() {
     return (
         <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">
-                            home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/pokemon">
-                            pokemon
-                        </Link>
-                    </li>
+            <nav className="navigation">
+                <ul className="navigation-list">
+                    <Link to="/">
+                    <li className="navigation-list-home">home</li>
+                    </Link>
+                    <Link to="/pokemon">
+                    <li className="navigation-list-pokemon">pokemon</li>
+                    </Link>
                 </ul>
             </nav>
         </header>
